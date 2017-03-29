566de53031abbda06ae332d39747ad97608a40bd
ikeikeikeike <jp.ne.co.jp@gmail.com>
2017-03-29 22:57:21 +0900

    unses fiel

---
+++ lib/panglao/tasks/cleanup.ex
@@ -0,0 +1,11 @@
+defmodule Panglao.Tasks.Cleanup do
+  def unses_files do
+    args = [ System.tmp_dir, "-maxdepth", "1" ,
+      "-type", "f",
+      "-user", System.get_env["USER"],
+      "-mmin", "+60",
+      "-exec", "rm", "{}", "\\;"]
+
+    :os.cmd 'find #{Enum.join args, " "}'
+  end
+end
