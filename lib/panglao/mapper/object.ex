236a500712285510c7e731dbcfdd2f064ae83c96
ikeikeikeike <jp.ne.co.jp@gmail.com>
2017-04-22 15:24:23 +0900

    modify download

--- lib/panglao/mapper/object.ex
+++ lib/panglao/mapper/object.ex
@@ -4,9 +4,9 @@ defmodule Panglao.Mapper.Object do
   def link(conn, %{object: o}) do
     %{
       object_status: o.stat,
-      embed: if(o.src, do: page_url(conn, :embed, o, o.src.file_name), else: error_url(conn, :unavailable)),
+      embed: if(o.src, do: page_url(conn, :embed, o, URI.encode(o.src.file_name)), else: error_url(conn, :unavailable)),
       short: if(o.src, do: page_url(conn, :short, o, o.slug), else: error_url(conn, :unavailable)),
-      direct: if(o.src, do: page_url(conn, :direct, o, o.src.file_name), else: error_url(conn, :unavailable)),
+      direct: if(o.src, do: page_url(conn, :direct, o, URI.encode(o.src.file_name)), else: error_url(conn, :unavailable)),
       updated_at: if(o.src, do: o.src.updated_at, else: o.updated_at),
       created_at: o.inserted_at,
     }
