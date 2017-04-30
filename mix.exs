9d52096609c18d2c4bb1daefff2ac2da82ee1c3c
ikeikeikeike <jp.ne.co.jp@gmail.com>
2017-04-30 21:43:06 +0900

    fallback

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Panglao.Mixfile do
   end
 
   defp version do
-    v = "0.2.14"
+    v = "0.2.16"
     File.write! "VERSION", v
     v
   end
--- web/controllers/api/v1/remote_controller.ex
+++ web/controllers/api/v1/remote_controller.ex
@@ -1,7 +1,7 @@
 defmodule Panglao.Api.V1.RemoteController do
   use Panglao.Web, :controller
 
-  alias Panglao.{Object.Remote, Object.Q, Client.Progress, Mapper, Hash, Object}
+  alias Panglao.{Object.Remote, Object.Q, Object.Progress, Mapper, Hash, Object}
 
   def upload(conn, %{"url" => url}) do
     user_id  = conn.assigns.current_user.id
@@ -22,26 +22,9 @@ defmodule Panglao.Api.V1.RemoteController do
 
   defp progress(conn, o) do
     r = Mapper.Object.link conn, %{object: o}
-    r = Map.merge r, %{
-      status: nil,
-      eta: nil,
-      speed: nil,
-      percent: nil,
-      total_bytes: nil,
-    }
+    b = Progress.get(o)
 
-    case Progress.get(o.url) do
-      {:ok, %{body: b}} ->
-        json conn, Map.merge(r, %{
-          status: Map.get(b, "status", "finished"),
-          eta: Map.get(b, "_eta_str"),
-          speed: Map.get(b, "_speed_str"),
-          percent: Map.get(b, "_percent_str"),
-          total_bytes: Map.get(b, "_total_bytes_estimate_str", Map.get(b, "_total_bytes_str")),
-        })
-      _ ->
-        json conn, r
-    end
+    json conn, Map.merge(r, b)
   end
 
 end
